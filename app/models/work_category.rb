24744fa04f5e684eb723a69b532e7bbd9aee0d0b
Chris007X <christophepoulain0102@gmail.com>
2026-03-10 13:53:40 +0100

    «associations_validations-users_work-categories»

--- app/models/work_category.rb
+++ app/models/work_category.rb
@@ -1,2 +1,18 @@
 class WorkCategory < ApplicationRecord
+   # Associations
+   has_many :work_items
+   has_many :materials
+   
+   # Validations
+  validates :name, presence: true, length: { minimum: 2 }
+  validates :slug, presence: true, uniqueness: true, format: { with: /\A[a-z0-9-]+\z/ }
+
+  # Petit bonus : s'assurer que le slug est toujours en minuscules avant la sauvegarde
+  before_validation :lowercase_slug
+
+  private
+
+  def lowercase_slug
+    self.slug = slug.downcase if slug.present?
+  end
 end
