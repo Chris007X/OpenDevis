2e40e6bb85a66e9655a3550b237ee561bd8fd404
Joseph Abisaleh <joseph.abisaleh@gmail.com>
2026-03-11 12:43:35 +0100

    latest

--- app/models/work_category.rb
+++ app/models/work_category.rb
@@ -1,6 +1,8 @@
 class WorkCategory < ApplicationRecord
   has_many :materials, dependent: :destroy
   has_many :work_items
+  has_many :artisan_categories, dependent: :destroy
+  has_many :artisans, through: :artisan_categories
 
   validates :name, presence: true
 end
