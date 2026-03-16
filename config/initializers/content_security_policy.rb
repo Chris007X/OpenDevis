b484211c04050d106c5977a970001b55ca2da45c
Joseph Abisaleh <joseph.abisaleh@gmail.com>
2026-03-16 12:29:49 +0100

    fixes

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -3,11 +3,11 @@
 Rails.application.configure do
   config.content_security_policy do |policy|
     policy.default_src :self
-    policy.font_src    :self, :data
+    policy.font_src    :self, :data, "https://fonts.gstatic.com"
     policy.img_src     :self, :data, :https, :blob      # :https pour og:image importées
     policy.object_src  :none
     policy.script_src  :self, :unsafe_inline            # requis importmap + Stimulus inline
-    policy.style_src   :self, :unsafe_inline            # requis Bootstrap + _od_styles inline
+    policy.style_src   :self, :unsafe_inline, "https://fonts.googleapis.com"  # requis Bootstrap + _od_styles inline + Google Fonts
     policy.connect_src :self,
                        "https://geo.api.gouv.fr",                       # city-autocomplete
                        "https://models.inference.ai.azure.com"          # LLM API (GitHub Models)
